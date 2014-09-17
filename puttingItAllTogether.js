0652ab745d7a87131195736da6567453e23cce72
Damian Esteban <damian.esteban@gmail.com>
2014-09-17 13:39:15 -0400

    finish tutorial

--- puttingItAllTogether.js
+++ puttingItAllTogether.js
@@ -1,16 +1,22 @@
-// Here is a bit of Meteor code from Microscope:
+// Here is a bit of Meteor code from Microscope.
+// ===========================================================
+// First, prevent the default browser behavior for the submit event.
 Template.postEdit.events({
   'submit form': function(event) {
     event.preventDefault();
 
+    // Then, define a variable containing the current post’s ID.
     var currentPostId = this._id;
 
+    // Get the latest value of the text fields.
     var postProperties = {
       url: $(event.target).find('[name=url]').val(),
       title: $(event.target).find('[name=url]').val(),
-    }
+    };
 
+    // Update the database with those values.
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      // Finally, either throw an error, or redirect the user.
       if (error) {
         // display the error to the user
         throwError(error.reason);
@@ -21,4 +27,4 @@ Template.postEdit.events({
   }
 });
 
-// TODO: Go over each line of code with comments.
+
